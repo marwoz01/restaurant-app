ee35939bbcbd4a7ad675ae7f310f743380170911
Marcel <146003678+marwoz01@users.noreply.github.com>
2025-05-19 10:10:32 +0200

    Add files via upload

---
+++ restaurant-app/index.js
@@ -0,0 +1,36 @@
+import { pulledPorkBurgers, smashBurgers, zapiekanki, dodatki, napoje } from './menu.js';
+
+function getMenuHtml(menuArr) {
+    return menuArr.map(menu => {
+        const { name, price, ingredients, image } = menu;
+        return `
+            <div class="card">
+                <img src="./images/${image}" alt="${name}">
+                <div class="card-content">
+                    <h2>${name}</h2>
+                    <p>${ingredients ? ingredients.join(", ") : ''}</p>
+                    <h3>${price}zł</h3>
+                </div>
+            </div>` 
+    }).join('');
+}
+
+document.querySelectorAll('.category').forEach(category => {
+    category.addEventListener('click', (e) => {
+        const selectedCategory = category.getAttribute('data-category');
+
+        if (selectedCategory === "pulledPorkBurgers") {
+            document.getElementById('container').innerHTML = getMenuHtml(pulledPorkBurgers);
+        } else if (selectedCategory === "smashBurgers") {
+            document.getElementById('container').innerHTML = getMenuHtml(smashBurgers);
+        } else if (selectedCategory === "zapiekanki") {
+            document.getElementById('container').innerHTML = getMenuHtml(zapiekanki);
+        } else if (selectedCategory === "dodatki") {
+            document.getElementById('container').innerHTML = getMenuHtml(dodatki);
+        } else if (selectedCategory === "napoje") {
+            document.getElementById('container').innerHTML = getMenuHtml(napoje);
+        }
+    });
+});
+
+document.getElementById('container').innerHTML = getMenuHtml(smashBurgers);
---
+++ restaurant-app/menu.js
@@ -0,0 +1,105 @@
+export const pulledPorkBurgers = [
+  {
+    name: "Rednek",
+    price: 39.00,
+    ingredients: ["pulled pork", "cheddar", "boczek x2", "pomidor", "pikle", "cebula", "sałata", "majonez", "sos BBQ"],
+    image: "/burger-rednek.jpg"
+  },
+  {
+    name: "Kozioł",
+    price: 39.00,
+    ingredients: ["pulled pork", "ser kozi", "burak", "pomidorki cherry", "rukola", "sałata", "sos kolendrowy"],
+    image: "/burger-koziol.jpg"
+  },
+  {
+    name: "Tłusty Papudrak",
+    price: 39.00,
+    ingredients: ["pulled pork", "cheddar x2", "boczek x2", "frytki", "krążki cebulowe", "pikle", "sos czosnkowy"],
+    image: "/burger-tlustypapudrak.jpg"
+  }
+];
+
+
+export const smashBurgers = [
+  {
+    name: "Smash Czizer",
+    price: 34.00,
+    ingredients: ["2x wołowina", "2x cheddar", "marynowana cebula", "sałata lodowa", "sos serowy", "chili jam"],
+    image: "/smash-czizer.jpg"
+  },
+  {
+    name: "Smash Dijonaise",
+    price: 35.00,
+    ingredients: ["2x wołowina", "2x cheddar", "krążki cebulowe", "pikle", "sałata lodowa", "sos Dijonaise"],
+    image: "/smash-dijonaise.jpg"
+  },
+  {
+    name: "Smash Cebulą",
+    price: 34.00,
+    ingredients: ["2x wołowina", "2x cheddar", "marynowana cebula", "grillowana cebula", "prażona cebulka", "sałata lodowa", "sos czosnkowy"],
+    image: "/smash-cebula.jpg"
+  }
+];
+
+
+export const zapiekanki = [
+  {
+    name: "Zapiekanka Włoska",
+    price: 33.00,
+    ingredients: ["pasta grzybowa", "ser", "pulled pork", "rukola", "pomidorki cherry", "sos kolendrowy"],
+    image: "/zapiekanka-wloska.jpg"
+  },
+  {
+    name: "Zapiekanka Włoska Wege",
+    price: 31.00,
+    ingredients: ["pasta grzybowa", "ser", "rukola", "pomidorki cherry", "sos kolendrowy"],
+    image: "/zapiekanka-wloskawege.jpg"
+  },
+  {
+    name: "Zapiekanka Swojska",
+    price: 34.00,
+    ingredients: ["pasta grzybowa", "ser", "pulled pork", "cebula", "boczek", "pikle", "prażona cebulka", "sos czosnkowy"],
+    image: "/zapiekanka-swojska.jpg"
+  }
+];
+
+
+export const dodatki = [
+  {
+    name: "Frytki",
+    price: 10.00,
+    image: "/dodatki-frytki.jpg"
+  },
+  {
+    name: "Frytki z batatów",
+    price: 15.00,
+    image: "/dodatki-frytkizbatatow.jpg"
+  },
+  {
+    name: "Krążki cebulowe",
+    price: 14.00,
+    image: "/dodatki-krazki.jpg"
+  }
+];
+
+
+export const napoje = [
+  {
+    name: "Pepsi",
+    price: 10.00,
+    volume: "0.33l",
+    image: "/napoje-lemoniada.png"
+  },
+  {
+    name: "Lemoniada On Lemon",
+    price: 14.00,
+    volume: "0.33l",
+    image: "/napoje-lemoniada.png"
+  },
+  {
+    name: "Woda niegazowana",
+    price: 6.00,
+    volume: "0.3l",
+    image: "/napoje-lemoniada.png"
+  }
+];
