9127f708ee3b531a13dbf3b57cf9e4251aa5ed78
Marcel <146003678+marwoz01@users.noreply.github.com>
2025-06-05 21:53:09 +0200

    Update menu.js

--- restaurant-app/menu.js
+++ restaurant-app/menu.js
@@ -1,105 +1,217 @@
 export const pulledPorkBurgers = [
   {
-    name: "Rednek",
-    price: 39.00,
-    ingredients: ["pulled pork", "cheddar", "boczek x2", "pomidor", "pikle", "cebula", "sałata", "majonez", "sos BBQ"],
-    image: "./burger-rednek.jpg"
-  },
-  {
     name: "Kozioł",
-    price: 39.00,
-    ingredients: ["pulled pork", "ser kozi", "burak", "pomidorki cherry", "rukola", "sałata", "sos kolendrowy"],
-    image: "./burger-koziol.jpg"
+    price: 39.0,
+    ingredients: [
+      "pulled pork",
+      "ser kozi",
+      "burak",
+      "pomidorki cherry",
+      "rukola",
+      "sałata",
+      "sos kolendrowy",
+    ],
+    image: "./burger-koziol.jpg",
   },
   {
     name: "Tłusty Papudrak",
-    price: 39.00,
-    ingredients: ["pulled pork", "cheddar x2", "boczek x2", "frytki", "krążki cebulowe", "pikle", "sos czosnkowy"],
-    image: "./burger-tlustypapudrak.jpg"
-  }
+    price: 39.0,
+    ingredients: [
+      "pulled pork",
+      "cheddar x2",
+      "boczek x2",
+      "frytki",
+      "krążki cebulowe",
+      "pikle",
+      "sos czosnkowy",
+    ],
+    image: "./burger-tlustypapudrak.jpg",
+  },
+  {
+    name: "Koleś",
+    price: 39.0,
+    ingredients: ["pulled pork", "colesław", "sałata", "sos chimichurri-majo"],
+    image: "./burger-rednek.jpg",
+  },
+  {
+    name: "Lazur",
+    price: 39.0,
+    ingredients: [
+      "pulled pork",
+      "ser pleśniowy blue",
+      "sałata",
+      "papryczka",
+      "rukola",
+      "sałata",
+      "sos truflowy",
+    ],
+    image: "./pulled-lazur.jpg",
+  },
+  {
+    name: "Cheese",
+    price: 39.0,
+    ingredients: [
+      "pulled pork",
+      "cheddar",
+      "pomidor",
+      "pikle",
+      "cebula",
+      "sałata",
+      "sos hamburgerowy i ketchup",
+    ],
+    image: "./pulled-cheese.jpg",
+  },
 ];
-
 
 export const smashBurgers = [
   {
     name: "Smash Czizer",
-    price: 34.00,
-    ingredients: ["2x wołowina", "2x cheddar", "marynowana cebula", "sałata lodowa", "sos serowy", "chili jam"],
-    image: "./smash-czizer.jpg"
+    price: 34.0,
+    ingredients: [
+      "2x wołowina",
+      "2x cheddar",
+      "marynowana cebula",
+      "sałata lodowa",
+      "sos serowy",
+      "chili jam",
+    ],
+    image: "./smash-czizer.jpg",
   },
   {
     name: "Smash Dijonaise",
-    price: 35.00,
-    ingredients: ["2x wołowina", "2x cheddar", "krążki cebulowe", "pikle", "sałata lodowa", "sos Dijonaise"],
-    image: "./smash-dijonaise.jpg"
+    price: 35.0,
+    ingredients: [
+      "2x wołowina",
+      "2x cheddar",
+      "krążki cebulowe",
+      "pikle",
+      "sałata lodowa",
+      "sos Dijonaise",
+    ],
+    image: "./smash-dijonaise.jpg",
   },
   {
     name: "Smash Cebulą",
-    price: 34.00,
-    ingredients: ["2x wołowina", "2x cheddar", "marynowana cebula", "grillowana cebula", "prażona cebulka", "sałata lodowa", "sos czosnkowy"],
-    image: "./smash-cebula.jpg"
-  }
+    price: 34.0,
+    ingredients: [
+      "2x wołowina",
+      "2x cheddar",
+      "marynowana cebula",
+      "grillowana cebula",
+      "prażona cebulka",
+      "sałata lodowa",
+      "sos czosnkowy",
+    ],
+    image: "./smash-cebula.jpg",
+  },
+  {
+    name: "Smash Bekon",
+    price: 35.0,
+    ingredients: [
+      "2x wołowina",
+      "2x cheddar",
+      "2x bekon",
+      "marynowana cebulka",
+      "pikle",
+      "sałata lodowa",
+      "sos bbq mayo",
+    ],
+    image: "./smash-bekon.jpg",
+  },
+  {
+    name: "Smash Triple",
+    price: 34.0,
+    ingredients: [
+      "3x wołowina",
+      "3x cheddar",
+      "3x bekon",
+      "pikle",
+      "cebula",
+      "sałata",
+      "sos hamburgerowy",
+    ],
+    image: "./smash-triple.jpg",
+  },
 ];
-
 
 export const zapiekanki = [
   {
     name: "Zapiekanka Włoska",
-    price: 33.00,
-    ingredients: ["pasta grzybowa", "ser", "pulled pork", "rukola", "pomidorki cherry", "sos kolendrowy"],
-    image: "./zapiekanka-wloska.jpg"
+    price: 33.0,
+    ingredients: [
+      "pasta grzybowa",
+      "ser",
+      "pulled pork",
+      "rukola",
+      "pomidorki cherry",
+      "sos kolendrowy",
+    ],
+    image: "./zapiekanka-wloska.jpg",
   },
   {
     name: "Zapiekanka Włoska Wege",
-    price: 31.00,
-    ingredients: ["pasta grzybowa", "ser", "rukola", "pomidorki cherry", "sos kolendrowy"],
-    image: "./zapiekanka-wloskawege.jpg"
+    price: 31.0,
+    ingredients: [
+      "pasta grzybowa",
+      "ser",
+      "rukola",
+      "pomidorki cherry",
+      "sos kolendrowy",
+    ],
+    image: "./zapiekanka-wloskawege.jpg",
   },
   {
     name: "Zapiekanka Swojska",
-    price: 34.00,
-    ingredients: ["pasta grzybowa", "ser", "pulled pork", "cebula", "boczek", "pikle", "prażona cebulka", "sos czosnkowy"],
-    image: "./zapiekanka-swojska.jpg"
-  }
+    price: 34.0,
+    ingredients: [
+      "pasta grzybowa",
+      "ser",
+      "pulled pork",
+      "cebula",
+      "boczek",
+      "pikle",
+      "prażona cebulka",
+      "sos czosnkowy",
+    ],
+    image: "./zapiekanka-swojska.jpg",
+  },
 ];
-
 
 export const dodatki = [
   {
     name: "Frytki",
-    price: 10.00,
-    image: "./dodatki-frytki.jpg"
+    price: 10.0,
+    image: "./dodatki-frytki.jpg",
   },
   {
     name: "Frytki z batatów",
-    price: 15.00,
-    image: "./dodatki-frytkizbatatow.jpg"
+    price: 15.0,
+    image: "./dodatki-frytkizbatatow.jpg",
   },
   {
     name: "Krążki cebulowe",
-    price: 14.00,
-    image: "./dodatki-krazki.jpg"
-  }
+    price: 14.0,
+    image: "./dodatki-krazki.jpg",
+  },
 ];
-
 
 export const napoje = [
   {
     name: "Pepsi",
-    price: 10.00,
+    price: 10.0,
     volume: "0.33l",
-    image: "./napoje-lemoniada.png"
+    image: "./napoje-lemoniada.png",
   },
   {
     name: "Lemoniada On Lemon",
-    price: 14.00,
+    price: 14.0,
     volume: "0.33l",
-    image: "./napoje-lemoniada.png"
+    image: "./napoje-lemoniada.png",
   },
   {
     name: "Woda niegazowana",
-    price: 6.00,
+    price: 6.0,
     volume: "0.3l",
-    image: "./napoje-lemoniada.png"
-  }
+    image: "./napoje-lemoniada.png",
+  },
 ];
